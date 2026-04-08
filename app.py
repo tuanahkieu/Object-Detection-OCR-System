625490ddc9479f1148c95570636c24234e0d5c5e
Tuấn Anh Kiều <kieutuananhh137@gmail.com>
2026-04-08 18:49:45 +0700

    Thêm requirements và cấu hình port cho Render

--- app.py
+++ app.py
@@ -142,5 +142,6 @@ def model_info():
     })
 
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5001, debug=False)
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
